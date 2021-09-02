756cd4c65da4c77e89af8e17b2cfebd9135ede95
snow <1184861291@qq.com>
2021-09-02 14:53:55 +0800

    modify some files

--- src/utils/multicall.ts
+++ src/utils/multicall.ts
@@ -15,7 +15,6 @@ interface MulticallOptions {
 const multicall = async <T = any>(abi: any[], calls: Call[]): Promise<T> => {
   try {
     const multi = getMulticallContract()
-    console.info('multi',multi);
     const itf = new ethers.utils.Interface(abi)
 
     const calldata = calls.map((call) => [call.address.toLowerCase(), itf.encodeFunctionData(call.name, call.params)])
