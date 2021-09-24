e1daff8b74685703320974ede2fd26a2c957251f
jaimetrovoada <jaimetrovoada@gmail.com>
2021-09-24 15:45:02 +0800

    Add increase/decrease buttons to leverageCell
    Might need to create a state to pass leverage set
    by the user to the Farm page. Another solution is
    to not set the leverage in the table and only set it
    in the next page (Farm)

--- src/views/Leverage/components/LeverageTable/Cells/LeverageCell.tsx
+++ src/views/Leverage/components/LeverageTable/Cells/LeverageCell.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { BIG_ZERO } from 'utils/bigNumber'
-import { Text, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { ChevronDownIcon, ChevronUpIcon, Flex, Input, Text, useMatchBreakpoints, Box, Button } from '@pancakeswap/uikit'
 import BigNumber from 'bignumber.js'
 import { Pool } from 'state/types'
 import { useTranslation } from 'contexts/Localization'
@@ -21,16 +21,54 @@ const StyledCell = styled(BaseCell)`
     }
   }
 `
+const LeverageContainer = styled(Flex)`
+  border-radius: 10px;
+  text-align: center;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors.cardBorder};
+`
+
+const CustomButton = styled(Button)`
+  border-radius: 0;
+  border: none;
+  border-left: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  padding: 0;
+  &:first-child {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  }
+`
 
 const LeverageCell = ({ leverage }) => {
   const { isMobile } = useMatchBreakpoints()
+  const [lvgValue, setLvgValue] = useState(leverage)
+  const increaseLvgValue = (e) => {
+    setLvgValue(lvgValue + 0.5)
+  }
+  const decreaseLvgValue = (e) => {
+    setLvgValue(lvgValue - 0.5)
+  }
+
   return (
     <StyledCell role="cell">
       <CellContent>
         <Text fontSize="12px" color="textSubtle" textAlign="left">
           Leverage
         </Text>
-        <Text>{leverage}</Text>
+        {/*   <Text>{lvgValue}</Text> */}
+        <LeverageContainer>
+          {/*  <CustomInput type="number" value={lvgValue} min="1" max="10" /> */}
+          <Flex padding="1rem">
+            <Text>{lvgValue.toFixed(2)}</Text>
+          </Flex>
+          <Flex flexDirection="column">
+            <CustomButton scale="xs" variant="secondary" onClick={increaseLvgValue} disabled={lvgValue === leverage}>
+              <ChevronUpIcon />
+            </CustomButton>
+            <CustomButton scale="xs" variant="secondary" onClick={decreaseLvgValue} disabled={lvgValue === 1}>
+              <ChevronDownIcon />
+            </CustomButton>
+          </Flex>
+        </LeverageContainer>
       </CellContent>
     </StyledCell>
   )
