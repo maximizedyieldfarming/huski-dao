c39f6fcde75278feffb3db6f310cad1a5c40709a
jaimetrovoada <jaimetrovoada@gmail.com>
2021-09-27 17:33:50 +0800

    Add i icon to display extra text/information

--- src/views/Leverage/components/LeverageTable/Cells/ApyCell.tsx
+++ src/views/Leverage/components/LeverageTable/Cells/ApyCell.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { BIG_ZERO } from 'utils/bigNumber'
-import { Skeleton, Text, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { Text, useMatchBreakpoints, Skeleton, Box, Flex, InfoIcon } from '@pancakeswap/uikit'
 import BigNumber from 'bignumber.js'
 import { Pool } from 'state/types'
 import { useTranslation } from 'contexts/Localization'
 import BaseCell, { CellContent } from './BaseCell'
+
+interface InfoParams {
+  show: boolean
+}
 
 const StyledCell = styled(BaseCell)`
   flex: 1 0 50px;
@@ -22,19 +26,69 @@ const StyledCell = styled(BaseCell)`
   }
 `
 
+const Info = styled(Box)<InfoParams>`
+  display: ${({ show }) => (show ? 'flex' : 'none')};
+  flex-direction: column;
+  position: absolute;
+  top: 2rem;
+  right: 2px;
+  ${({ theme }) => theme.mediaQueries.xl} {
+    right: unset;
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
+  padding: 1rem;
+  gap: 10px;
+  background-color: ${({ theme }) => theme.colors.background};
+  box-shadow: ${({ theme }) => theme.card.boxShadow};
+  border-radius: ${({ theme }) => theme.radii.default};
+  z-index: ${({ theme }) => theme.zIndices.modal};
+  /*  ${Text} {
+    word-wrap: none;
+    word-break: keep-all;
+  }
+  width: max-content; */
+  ${Flex} {
+    gap: 10px;
+  }
+`
+
 const ApyCell = ({ apy }) => {
+  const [displayInfo, setDisplayInfo] = useState(false)
+  const changeDisplayInfo = (e) => setDisplayInfo(!displayInfo)
   const { isMobile } = useMatchBreakpoints()
+
+  const showText = (() => (
+    <>
+      <Text>{apy}%</Text>
+      <Box
+        onMouseEnter={changeDisplayInfo}
+        onMouseLeave={changeDisplayInfo}
+        position="relative"
+        style={{ cursor: 'pointer' }}
+      >
+        <InfoIcon ml="10px" />
+        <Info show={displayInfo}>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Text small>Lending&nbsp;APR</Text>
+            <Skeleton width="80px" height="16px" />
+          </Flex>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Text small>Staking&nbsp;APR</Text>
+            <Skeleton width="80px" height="16px" />
+          </Flex>
+        </Info>
+      </Box>
+    </>
+  ))()
+
   return (
     <StyledCell role="cell">
       <CellContent>
         <Text fontSize="12px" color="textSubtle" textAlign="left">
           Apy
         </Text>
-        {apy ? (
-          <Text>{apy}%</Text>
-        ) : (
-          <Skeleton width="80px" height="16px" />
-        )}
+        <Flex alignItems="center">{apy ? showText : <Skeleton width="80px" height="16px" />}</Flex>
       </CellContent>
     </StyledCell>
   )
