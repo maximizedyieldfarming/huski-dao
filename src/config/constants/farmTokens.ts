30d2dfa90091127cde04a49b9048901db9946d2b
snow <1184861291@qq.com>
2021-09-29 18:53:07 +0800

    rename cake token

--- src/config/constants/farmTokens.ts
+++ src/config/constants/farmTokens.ts
@@ -3,7 +3,7 @@ import { Token } from './types'
 
 const farmTokens: Token[] = [
   {
-    symbol: 'CAKE3456',
+    symbol: 'CAKE',
     address: {
       56: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
       97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -65,7 +65,7 @@ const tokens = {
     projectLink: 'https://www.binance.com/',
   },
   cake: {
-    symbol: 'CAKE3456',
+    symbol: 'CAKE',
     address: {
       56: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
       97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
