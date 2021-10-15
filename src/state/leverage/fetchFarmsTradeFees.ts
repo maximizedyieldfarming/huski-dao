d0435557a3f9bb12e788c85546755f0f7620c4a3
snow <1184861291@qq.com>
2021-10-15 18:57:14 +0800

    del cross domain

--- src/state/leverage/fetchFarmsTradeFees.ts
+++ src/state/leverage/fetchFarmsTradeFees.ts
@@ -1,18 +1,19 @@
 
 const fetchFarmsTradeFees = async (farms) => {
 
-  const header = new Headers({
-    'Access-Control-Allow-Origin':'*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Content-Type':'application/json;charset=utf8',
-    "contentType": "application/json;charset=utf-8"
- })
+//   const header = new Headers({
+//     'Access-Control-Allow-Origin':'*',
+//     'Access-Control-Allow-Credentials': 'true',
+//     'Content-Type':'application/json;charset=utf8',
+//     "contentType": "application/json;charset=utf-8"
+//  })
 
-  const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee', {
-    method: 'GET',
-    headers: header,
-    mode: 'no-cors'
-  })
+  // const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee', {
+  //   method: 'GET',
+  //   headers: header,
+  //   mode: 'no-cors'
+  // })
+  const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee')
   const responseData = await response.json()
   const tradeFeeData = responseData.data.tradingFees;
 
