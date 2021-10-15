353d3da5414099bd3b606f388cd2607040f7e71e
snow <1184861291@qq.com>
2021-10-15 17:49:28 +0800

    to solve cors problem, but can not show data

--- src/state/leverage/fetchFarmsTradeFees.ts
+++ src/state/leverage/fetchFarmsTradeFees.ts
@@ -1,9 +1,21 @@
 
 const fetchFarmsTradeFees = async (farms) => {
-  const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee')
+
+  const header = new Headers({
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Content-Type':'application/json;charset=utf8',
+    "contentType": "application/json;charset=utf-8"
+ })
+
+  const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee', {
+    method: 'GET',
+    headers: header,
+    mode: 'no-cors'
+  })
   const responseData = await response.json()
   const tradeFeeData = responseData.data.tradingFees;
- 
+
   const farmsWithTradeFees = farms.map((farm) => {
     let tradeFee = 0;
     // eslint-disable-next-line array-callback-return
