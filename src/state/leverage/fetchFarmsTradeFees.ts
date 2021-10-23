c7ac32da0b1ab6fcc079eec3b4afd90b4c28ee36
snow <1184861291@qq.com>
2021-10-23 12:10:13 +0800

    commit alpaca test api

--- src/state/leverage/fetchFarmsTradeFees.ts
+++ src/state/leverage/fetchFarmsTradeFees.ts
@@ -1,19 +1,36 @@
 const fetchFarmsTradeFees = async (farms) => {
 
-  const response = await fetch('http://192.168.0.187:8383/api/v1/tradingfees/get?dex=PancakeSwap')
-  const responseData = await response.json()
-  const tradeFeeData = responseData.data;
 
-  const farmsWithTradeFees = farms.map((farm) => {
-    let tradeFee = 0;
-    // eslint-disable-next-line array-callback-return
-    tradeFeeData.map((tf) => {
-      if (farm.lpAddresses[56].toUpperCase() === tf.LpAddress.toUpperCase()) {
-        tradeFee = tf.DailyTradingFeesApr;
-      }
+  // use alpaca api
+    const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee')
+    const responseData = await response.json()
+    const tradeFeeData = responseData.data.tradingFees;
+    const farmsWithTradeFees = farms.map((farm) => {
+      let tradeFee = 0;
+      // eslint-disable-next-line array-callback-return
+      tradeFeeData.map((tf) => {
+          if (farm.lpAddresses[56].toUpperCase() === tf.lpAddress.toUpperCase()) {
+          tradeFee = tf.dailyTradingFeesApr
+        }
+      })
+      return { ...farm, tradeFee }
     })
-    return { ...farm, tradeFee }
-  })
+
+
+    // use test
+  // const response = await fetch('http://192.168.0.187:8383/api/v1/tradingfees/get?dex=PancakeSwap')
+  // const responseData = await response.json()
+  // const tradeFeeData = responseData.data;
+  // const farmsWithTradeFees = farms.map((farm) => {
+  //   let tradeFee = 0;
+  //   // eslint-disable-next-line array-callback-return
+  //   tradeFeeData.map((tf) => {
+  //     if (farm.lpAddresses[56].toUpperCase() === tf.LpAddress.toUpperCase()) {
+  //       tradeFee = tf.DailyTradingFeesApr;
+  //     }
+  //   })
+  //   return { ...farm, tradeFee }
+  // })
 
   return farmsWithTradeFees
 }
