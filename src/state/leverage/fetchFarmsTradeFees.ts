a4de813d4ec713f6e928a659f6e16c8b6dd5efe2
snow <1184861291@qq.com>
2021-10-26 10:37:25 +0800

    add test tradefee env

--- src/state/leverage/fetchFarmsTradeFees.ts
+++ src/state/leverage/fetchFarmsTradeFees.ts
@@ -1,36 +1,57 @@
+/* eslint-disable array-callback-return */
+const fetchFarmsTradeFeesForAlpaca = async (farms) => {
+
+  const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee')
+  const responseData = await response.json()
+  const tradeFeeData = responseData.data.tradingFees;
+  const farmsWithTradeFees = farms.map((farm) => {
+    let tradeFee = 0;
+    tradeFeeData.map((tf) => {
+      if (farm.lpAddresses[56].toUpperCase() === tf.lpAddress.toUpperCase()) {
+        tradeFee = tf.dailyTradingFeesApr
+      }
+    })
+    return { ...farm, tradeFee }
+  })
+
+  return farmsWithTradeFees
+}
+
+const fetchFarmsTradeFeesForTest = async (farms) => {
+
+  const response = await fetch('http://192.168.0.187:8383/api/v1/tradingfees/get?dex=PancakeSwap')
+  const responseData = await response.json()
+  const tradeFeeData = responseData.data;
+  const farmsWithTradeFees = farms.map((farm) => {
+    let tradeFee = 0;
+    tradeFeeData.map((tf) => {
+      if (farm.lpAddresses[56].toUpperCase() === tf.LpAddress.toUpperCase()) {
+        tradeFee = tf.DailyTradingFeesApr;
+      }
+    })
+    return { ...farm, tradeFee }
+  })
+
+  return farmsWithTradeFees
+}
+
+
 const fetchFarmsTradeFees = async (farms) => {
 
-
-  // use alpaca api
-    const response = await fetch('https://api.alpacafinance.org/v1/internal/tradingFee')
-    const responseData = await response.json()
-    const tradeFeeData = responseData.data.tradingFees;
-    const farmsWithTradeFees = farms.map((farm) => {
-      let tradeFee = 0;
-      // eslint-disable-next-line array-callback-return
-      tradeFeeData.map((tf) => {
-          if (farm.lpAddresses[56].toUpperCase() === tf.lpAddress.toUpperCase()) {
-          tradeFee = tf.dailyTradingFeesApr
-        }
-      })
-      return { ...farm, tradeFee }
-    })
-
-
-    // use test
-  // const response = await fetch('http://192.168.0.187:8383/api/v1/tradingfees/get?dex=PancakeSwap')
-  // const responseData = await response.json()
-  // const tradeFeeData = responseData.data;
-  // const farmsWithTradeFees = farms.map((farm) => {
-  //   let tradeFee = 0;
-  //   // eslint-disable-next-line array-callback-return
-  //   tradeFeeData.map((tf) => {
-  //     if (farm.lpAddresses[56].toUpperCase() === tf.LpAddress.toUpperCase()) {
-  //       tradeFee = tf.DailyTradingFeesApr;
-  //     }
-  //   })
-  //   return { ...farm, tradeFee }
-  // })
+  let farmsWithTradeFees;
+  const env = process.env.REACT_APP_ENV;
+  switch (env) {
+    case 'dev':
+      farmsWithTradeFees = fetchFarmsTradeFeesForAlpaca(farms)
+      break;
+    case 'test':
+      farmsWithTradeFees = fetchFarmsTradeFeesForTest(farms);
+      break;
+    case 'prod':
+    default:
+      farmsWithTradeFees = fetchFarmsTradeFeesForAlpaca(farms);
+      break;
+  }
 
   return farmsWithTradeFees
 }
