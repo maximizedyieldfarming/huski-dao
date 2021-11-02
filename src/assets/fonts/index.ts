be03e96e75e057a64261e8ca610ff90187cae8fd
jaimetrovoada <jaimetrovoada@gmail.com>
2021-11-02 19:56:27 +0800

    Replace font files + module declarations

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,5 @@
+import(/* webpackPreload: true */ 'assets/fonts')
+
+export {default as RegularFont} from './GenJyuuGothic-P-Regular.woff'
+export { default as BoldFont } from './GenJyuuGothic-Bold.woff'
+export {default as HeavyFont} from './GenJyuuGothic-P-Heavy.woff'
