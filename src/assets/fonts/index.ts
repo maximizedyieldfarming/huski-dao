be8ae4eb4e0144ba771951cfb3d9ab16dcfcb7ff
jaimetrovoada <jaimetrovoada@gmail.com>
2021-11-03 17:45:09 +0800

    font files

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -1,5 +1,3 @@
 import(/* webpackPreload: true */ 'assets/fonts')
 
 export {default as RegularFont} from './GenJyuuGothic-P-Regular.woff'
-export { default as BoldFont } from './GenJyuuGothic-Bold.woff'
-export {default as HeavyFont} from './GenJyuuGothic-P-Heavy.woff'
--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
 import { PancakeTheme } from '@pancakeswap/uikit/dist/theme'
+import { RegularFont } from 'assets/fonts'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
@@ -8,8 +9,13 @@ declare module 'styled-components' {
 }
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+        font-family: 'GenJyuuGothic Regular';
+        src: url(${RegularFont}) format('woff');
+    }
   * {
-    font-family: 'Kanit', sans-serif;
+    // font-family: 'Kanit', sans-serif;
+    font-family: 'GenJyuuGothic Regular';
   }
   body {
     background-color: ${({ theme }) => theme.colors.background};
