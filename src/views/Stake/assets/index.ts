50620d740a3dc4ec711e34a3ec9589eeacd640eb
jaimetrovoada <jaimetrovoada@gmail.com>
2021-11-05 16:04:03 +0800

    delete unused files

--- src/views/Stake/assets/index.ts
+++ src/views/Stake/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as WalletIcon } from './WalletIcon';
 export {default as LockIcon} from './LockIcon';
+export {default as FlexingHuski} from './stake_rewards_img.png'
+export {default as Bone} from './bone2-1x.png'
