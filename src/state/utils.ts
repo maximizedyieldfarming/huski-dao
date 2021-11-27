c0802afa99bd3987cd1d66be665560bc848ad48e
snow <1184861291@qq.com>
2021-11-27 17:55:26 +0800

    del useless code

--- src/state/utils.ts
+++ src/state/utils.ts
@@ -3,40 +3,6 @@ import getDomain from '../utils/env';
 
 const web3 = new Web3(getDomain());
 
-// export const fetchCakePrice = async () => {
-//   const cakePriceCoinGeckoApi = `https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token&vs_currencies=usd`;
-//   const res = await fetch(cakePriceCoinGeckoApi);
-//   const cakePrice = await res.json();
-//   return cakePrice['pancakeswap-token'].usd;
-// };
-
-// export const truncateAddress = (account, left = 4, right = 4) => {
-//   if (typeof account !== 'string') {
-//     account.toString();
-//   }
-//   const first = account.slice(0, left);
-//   const last = account.slice(-right);
-//   return first.concat('...', last);
-// };
-
-// export const copyToClipboard = (id) => {
-//   const r = document.createRange();
-//   r.selectNode(document.getElementById(id));
-//   window.getSelection().removeAllRanges();
-//   window.getSelection().addRange(r);
-//   document.execCommand('copy');
-//   window.getSelection().removeAllRanges();
-// };
-
-// export const splitPairs = (pair) => {
-//   return pair.toLowerCase().split('-');
-// };
-
-// export const renderPairsAvatars = (pair, n, Icons) => {
-//   const [pair1, pair2] = splitPairs(pair);
-//   return n === 1 ? Icons[pair1] : Icons[pair2];
-// };
-
 export const formatBigNumber = (value) => {
   return parseInt(web3.utils.fromWei(BigInt(value).toString())).toLocaleString(
     'en-US'
@@ -54,15 +20,3 @@ export const formatPercentage = (value) => {
     maximumFractionDigits: 2,
   });
 };
-
-
-// export const sumTokenData = (data) => {
-//       let finalResult;
-//       // eslint-disable-next-line prefer-const
-//       finalResult = data
-//             // eslint-disable-next-line no-useless-escape
-//             .map((pool) => parseFloat(formatBigNumber(pool).replace(/\,/g, '')))
-//             .reduce((cur, acc) => cur + acc)
-//             .toLocaleString()
-//       return finalResult;
-//   };
--- src/utils/formatDisplayedBalance.ts
+++ src/utils/formatDisplayedBalance.ts
@@ -3,11 +3,11 @@ import BigNumber from "bignumber.js";
 export const formatDisplayedBalance = (balance: string | number, decimalsDigits: number): string => {
   // will always display user balance rounded down
   const balanceBigNumber = new BigNumber(balance);
- let balanceNumber
-   if (balanceBigNumber.gt(balanceBigNumber.toFixed(decimalsDigits))) {
-     balanceNumber = balanceBigNumber.toFixed(decimalsDigits || 2, 1)
-    } else {
-      balanceNumber = balanceBigNumber.toFixed(2, 1)
-} 
+  let balanceNumber
+  if (balanceBigNumber.gt(balanceBigNumber.toFixed(decimalsDigits))) {
+    balanceNumber = balanceBigNumber.toFixed(decimalsDigits || 2, 1)
+  } else {
+    balanceNumber = balanceBigNumber.toFixed(2, 1)
+  }
   return balanceNumber;
 }
