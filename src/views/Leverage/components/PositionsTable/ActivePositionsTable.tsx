1e5ac4b8ee5931a5d75bff8d012137c622ddc0d8
jaimetrovoada <jaimetrovoada@gmail.com>
2021-12-22 17:11:08 +0800

    replace no posiitons with loader

--- src/views/Leverage/components/PositionsTable/ActivePositionsTable.tsx
+++ src/views/Leverage/components/PositionsTable/ActivePositionsTable.tsx
@@ -1,12 +1,13 @@
 import React, { useRef } from 'react'
 import styled from 'styled-components'
-import { Text, useMatchBreakpoints, Box } from 'husky-uikit1.0'
+import { useMatchBreakpoints, Grid, Skeleton, Flex, Box, Text } from 'husky-uikit1.0'
 import { useTranslation } from 'contexts/Localization'
+import { useLocation } from 'react-router-dom'
 import ActivePositionsRow from './ActivePositionsRow'
 import ActivePositionsHeaderRow from './ActivePositionsHeaderRow'
 
 const StyledTable = styled.div`
-  border-radius: ${({ theme }) => theme.radii.card};
+  // border-radius: ${({ theme }) => theme.radii.card};
   overflow: auto;
   height: 300px;
   ${({ theme }) => theme.mediaQueries.lg} {
@@ -22,20 +23,58 @@ const StyledTable = styled.div`
 `
 
 const ActivePositionsTable = ({ positionFarmsData }) => {
-  const tableWrapperEl = useRef<HTMLDivElement>(null)
-  const scrollToTop = (): void => {
-    tableWrapperEl.current.scrollIntoView({
-      behavior: 'smooth',
-    })
-  }
   const { t } = useTranslation()
   const { isMobile, isTablet } = useMatchBreakpoints()
+  const isSmallScreen = isMobile || isTablet
+  const { pathname } = useLocation()
+
+  const [isLoading, setIsLoading] = React.useState(true)
+  React.useEffect(() => {
+    setTimeout(() => {
+      if (positionFarmsData.length === 0) {
+        setIsLoading(false)
+      }
+    }, 8000)
+  })
+
+  const loader = (() => {
+    if (isSmallScreen) {
+      return (
+        <>
+          {[...Array(4)].map((_, i) => (
+            <Flex justifyContent="space-between" alignItems="center" padding="1rem 0">
+              <Skeleton key={_} width={i % 2 === 0 ? '200px' : '90px'} height="2rem" />
+              <Skeleton key={_} width={i % 2 === 0 ? '90px' : '200px'} height="2rem" />
+            </Flex>
+          ))}
+          <Skeleton width="100%" height="2rem" />
+        </>
+      )
+    }
+    return (
+      <>
+        <Grid
+          gridTemplateColumns={pathname === 'farms' ? 'repeat(10, 1fr)' : 'repeat(8, 1fr)'}
+          justifyContent="space-between"
+          padding="1rem 0"
+        >
+          {[...Array(pathname === 'farms' ? 10 : 8)].map((_, i) => (
+            <Skeleton key={_} width="80px" height="1rem" />
+          ))}
+        </Grid>
+        <Skeleton width="100%" height="3rem" marginBottom="1rem" />
+        <Skeleton width="100%" height="3rem" />
+      </>
+    )
+  })()
 
   return (
-    <StyledTable role="table" ref={tableWrapperEl}>
+    <StyledTable role="table">
       {!(isMobile || isTablet) && positionFarmsData?.length ? <ActivePositionsHeaderRow /> : null}
       {positionFarmsData?.length ? (
         positionFarmsData.map((pd) => <ActivePositionsRow data={pd} key={pd?.positionId} />)
+      ) : isLoading ? (
+        loader
       ) : (
         <Box padding="100px">
           <Text textAlign="center">{t('No Active Positions')}</Text>
--- src/views/Leverage/components/PositionsTable/LiquidatedPositionsTable.tsx
+++ src/views/Leverage/components/PositionsTable/LiquidatedPositionsTable.tsx
@@ -1,13 +1,14 @@
 import React, { useRef } from 'react'
 import styled from 'styled-components'
-import { Text, useMatchBreakpoints, Box } from 'husky-uikit1.0'
+import { useMatchBreakpoints, Grid, Skeleton, Flex, Box, Text } from 'husky-uikit1.0'
 import { useTranslation } from 'contexts/Localization'
+import { useLocation } from 'react-router-dom'
 import LiquidatedPositionsRow from './LiquidatedPositionsRow'
 import LiquidatedPositionsHeaderRow from './LiquidatedPositionsHeaderRow'
 
 const StyledTable = styled.div`
-  border-radius: ${({ theme }) => theme.radii.card};
-  overflow: hidden;
+  // border-radius: ${({ theme }) => theme.radii.card};
+  overflow: auto;
   height: 300px;
   ${({ theme }) => theme.mediaQueries.lg} {
     height: unset;
@@ -21,31 +22,59 @@ const StyledTable = styled.div`
   }
 `
 
-const StyledTableBorder = styled.div`
-  overflow: hidden;
-  border-radius: ${({ theme }) => theme.radii.card};
-  background-color: ${({ theme }) => theme.colors.cardBorder};
-  padding: 1px 1px 3px 1px;
-  background-size: 400% 400%;
-`
-
 const LiquidatedPositionsTable = ({ data }) => {
-  console.log('liquidated pos', data)
-  const tableWrapperEl = useRef<HTMLDivElement>(null)
-  const scrollToTop = (): void => {
-    tableWrapperEl.current.scrollIntoView({
-      behavior: 'smooth',
-    })
-  }
-
   const { t } = useTranslation()
   const { isMobile, isTablet } = useMatchBreakpoints()
 
+  const isSmallScreen = isMobile || isTablet
+  const { pathname } = useLocation()
+
+  const [isLoading, setIsLoading] = React.useState(true)
+  React.useEffect(() => {
+    setTimeout(() => {
+      if (data.length === 0) {
+        setIsLoading(false)
+      }
+    }, 8000)
+  })
+
+  const loader = (() => {
+    if (isSmallScreen) {
+      return (
+        <>
+          {[...Array(4)].map((_, i) => (
+            <Flex justifyContent="space-between" alignItems="center" padding="1rem 0">
+              <Skeleton key={_} width={i % 2 === 0 ? '200px' : '90px'} height="2rem" />
+              <Skeleton key={_} width={i % 2 === 0 ? '90px' : '200px'} height="2rem" />
+            </Flex>
+          ))}
+          <Skeleton width="100%" height="2rem" />
+        </>
+      )
+    }
+    return (
+      <>
+        <Grid
+          gridTemplateColumns={pathname === 'farms' ? 'repeat(10, 1fr)' : 'repeat(8, 1fr)'}
+          justifyContent="space-between"
+          padding="1rem 0"
+        >
+          {[...Array(pathname === 'farms' ? 10 : 8)].map((_, i) => (
+            <Skeleton key={_} width="80px" height="1rem" />
+          ))}
+        </Grid>
+        <Skeleton width="100%" height="3rem" marginBottom="1rem" />
+        <Skeleton width="100%" height="3rem" />
+      </>
+    )
+  })()
   return (
-    <StyledTable role="table" ref={tableWrapperEl}>
+    <StyledTable role="table">
       {!(isMobile || isTablet) && data && <LiquidatedPositionsHeaderRow />}
       {data ? (
         <LiquidatedPositionsRow data={data} />
+      ) : isLoading ? (
+        loader
       ) : (
         <Box padding="100px">
           <Text textAlign="center">{t('No Liquidated Positions')}</Text>
