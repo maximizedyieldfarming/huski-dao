5058a0360f83bd5911a0fb6890ced3625f61b730
snow <1184861291@qq.com>
2022-01-27 19:35:34 +0800

    del useless code

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -3,16 +3,4 @@ export const GRAPH_API_PREDICTION = process.env.REACT_APP_GRAPH_API_PREDICTION
 export const GRAPH_API_LOTTERY = process.env.REACT_APP_GRAPH_API_LOTTERY
 export const SNAPSHOT_VOTING_API = process.env.REACT_APP_SNAPSHOT_VOTING_API
 export const SNAPSHOT_BASE_URL = process.env.REACT_APP_SNAPSHOT_BASE_URL
-export const SNAPSHOT_API = `${SNAPSHOT_BASE_URL}/graphql`
-export const SNAPSHOT_HUB_API = `${SNAPSHOT_BASE_URL}/api/message`
-export const BITQUERY_API = 'https://graphql.bitquery.io'
 
-export const TRADING_FEE_URL =  'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
-export const VOLUME_24H = 'http://thegraph.huski.finance/subgraphs/name/huskifinance/Vault'
-// 'http://66.42.51.70:8000/subgraphs/name/huskyfarm/Vault'
-
-
-/**
- * V1 will be deprecated but is still used to claim old rounds
- */
-export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -14,28 +14,14 @@ export const getCakeAddress = () => {
 export const getFairLaunchAddress = () => {
   return getAddress(addresses.fairLaunch)
 }
-export const getHuskiAddress = () => {
 
-  let address
-  if (process.env.REACT_APP_CHAIN_ID === '97') {
-    address = getAddress(tokens.huski.address)
-  } else {
-    address = getAddress(tokens.alpaca.address)
-  }
-  return address
-}
-export const getsHuskiAddress = () => {
-  return getAddress(tokens.salpaca.address)
-}
 export const getMasterChefAddress = () => {
   return getAddress(addresses.masterChef)
 }
 export const getMulticallAddress = () => {
   return getAddress(addresses.multiCall)
 }
-export const getWbnbAddress = () => {
-  return getAddress(tokens.wbnb.address)
-}
+
 export const getPancakeProfileAddress = () => {
   return getAddress(addresses.pancakeProfile)
 }
