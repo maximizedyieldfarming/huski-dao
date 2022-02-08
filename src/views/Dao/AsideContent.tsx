fcc3afd696a923f05f913900db770b69082f1547
jaimetrovoada <jaimetrovoada@gmail.com>
2022-02-08 18:22:20 +0800

    fix container sizes

--- src/views/Dao/AsideContent.tsx
+++ src/views/Dao/AsideContent.tsx
@@ -81,7 +81,7 @@ const AsideContent = () => {
   const isSmallScreen = isMobile || isTablet
   return (
     <>
-      <Container>
+      <Container maxWidth={586}>
         <Flex alignItems="flex-end" justifyContent="space-between" mb="0 !important" flexWrap="wrap">
           <Text fontSize="24px !important" fontWeight={900} style={{ alignSelf: 'flex-start', whiteSpace: 'nowrap' }}>
             What is Huski DAO ?
