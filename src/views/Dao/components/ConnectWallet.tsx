8d6d3edfacde4cd76270525a588f8bc5e074a68f
jaimetrovoada <jaimetrovoada@gmail.com>
2022-02-10 16:54:40 +0800

    change network select options

--- src/views/Dao/components/ConnectWallet.tsx
+++ src/views/Dao/components/ConnectWallet.tsx
@@ -3,45 +3,16 @@ import { useWeb3React } from '@web3-react/core'
 import { useWalletModal, useMatchBreakpoints, Box, Text } from '@huskifinance/huski-frontend-uikit'
 import useAuth from 'hooks/useAuth'
 import UserMenu from 'components/UserMenu'
-// import { useHover } from '../helpers'
 import { StyledButton } from './styles'
 
 const ConnectWallet = (props) => {
   const { login, logout } = useAuth()
-  // const hasProvider: boolean = !!window.ethereum || !!window.BinanceChain
   const hasProvider = !!window.ethereum
-  // console.info('hasProvider', hasProvider)
-  // console.info('!!window.ethereum', !!window.ethereum)
-  // console.info('!!window.BinanceChain', window)
   const { onPresentConnectModal } = useWalletModal(login, logout, hasProvider)
   const { account } = useWeb3React()
 
-  // if( window.BinanceChain ){
-  //   alert('qinghuan wangluo')
-  //
-  //    }
-
-  // const [buttonIsHovering, buttonHoverProps] = useHover()
-  // hide this to test normal wllet connect button
-  // if (!account) {
-  //   return (
-  //     <StyledButton
-  //       onClick={(e) => e.preventDefault()}
-  //       {...props}
-  //       maxWidth={146}
-  //       height="100%"
-  //       {...buttonHoverProps}
-  //       style={{ cursor: 'not-allowed' }}
-  //     >
-  //       <Text fontWeight={700} style={{ whiteSpace: 'nowrap' }}>
-  //         {buttonIsHovering ? 'Coming Soon' : 'Connect Wallet'}
-  //       </Text>
-  //     </StyledButton>
-  //   )
-  // }
   const { isMobile } = useMatchBreakpoints()
-  // uncomment this to enable normal button
-  // product manager asked to disable this button while we are working on functionality
+
   if (!account) {
     return (
       <Box
